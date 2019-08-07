71a2841a59903bda01fe4ece1b5d947659cc2657
goodkorning <rnrrhs963@naver.com>
2019-08-07 20:59:46 +0900

    isolate_user2 makes vector i, j

--- data_processer.py
+++ data_processer.py
@@ -11,3 +11,12 @@ def data_reader () : #read rating file
         sp = line.split('::') ##parsing lines into [userid, movieid,rating, timestamp]
         user_item_matrix[int(sp[0])-1][int(sp[1])-1] = int(sp[2])
     return user_item_matrix
+
+def isolate_user2 (i, j) : ##list
+    uim = data_reader()
+    vector_i = []
+    vector_j = []
+    for rows in uim :
+        if((rows[i-1] != 0) & (rows[j-1] != 0)) :
+            vector_i.append(rows[i-1])
+            vector_j.append(rows[j-1])
